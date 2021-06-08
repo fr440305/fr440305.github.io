19564fcd0102c146d834b9f5f4cd6d71c8f41157
fr440305 <fr440305@gmail.com>
2021-06-08 12:33:57 -0700

    [[FEAT]] - Add toggle night mode button.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,23 @@
  * Created At: 8 Jun 2021
  */
 
-if (window.localStorage !== undefined) {
-	// supported
-	if (window.localStorage.getItem("nightmode") === "true") {
+if (window.localStorage !== undefined) { // supported
+	var at_night = window.localStorage.getItem("nightmode");
+
+	if (at_night === "true") {
 		document.body.classList.add("nightmode");
 	} else {
-		document.body.classList.remove("nightmode");
+		at_night = "false";
 	}
+
+	var el = document.getElementById("ElemToggleNightMode");
+	el.innerText = "Toggle Night Mode";
+	el.addEventListener("click", function() {
+		at_night = {
+			"true": "false",
+			"false": "true"
+		}[at_night];
+		window.localStorage.setItem("nightmode", at_night);
+		document.body.classList.toggle("nightmode");
+	});
 }
