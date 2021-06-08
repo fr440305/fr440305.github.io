f5f44be80f9eeefbc90b940153b2082ff10b9317
fr440305 <fr440305@gmail.com>
2021-06-08 01:08:02 -0700

    [[NEW FEAT]] - Night mode beta test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-// nothing here yet.
+/* {{.dist}}/js/scripts.js */
+
+/**
+ * Created At: 8 Jun 2021
+ */
+
+if (window.localStorage !== undefined) {
+	// supported
+	if (window.localStorage.getItem("nightmode") === "true") {
+		document.body.classList.add("nightmode");
+	} else {
+		document.body.classList.remove("nightmode");
+	}
+}
